67a2ecdd68b525d784e99ab7388b2cdb061510cf
noakarhu <59644045+noakarhu@users.noreply.github.com>
2020-12-09 18:25:29 +0200

    SELKEEMPI VERSIO:D

--- taloyhtio/src/App.js
+++ taloyhtio/src/App.js
@@ -1,5 +1,35 @@
-//import React from 'react'
+import React from 'react'
+import './muotoilut.css'
 
+const App = () => {
 
+return (
+    <div>
+        <h1>Otsikko tulee tähän!</h1>
 
-//export default App
+        <p>Sovellus tulostaa maalitarpeen litroina ja tämän litramäärän hinnan euroina koko taloyhtiölle, kullekkin asunnolle ja asuntojen eri tiloille.</p>
+        <p> </p>
+        <h2>Käytä maalilaskuria:</h2>
+
+        <div id="paintForm">
+            <form>
+                <label htmlFor="paintName">Maalin nimi:</label> <br/>
+                <input type="text" id="paintName" name="paintName"/> <br/> <br/>
+
+                <label htmlFor="paintPrice">Maalin hinta (€/l):</label> <br/>
+                <input type="text" id="paintPrice" name="paintPrice"/> <br/> <br/>
+
+                <label htmlFor="paintVolume">Maalin riittoisuus (m<sup>2</sup>/l):</label> <br/>
+                <input type="text" id="paintVolume" name="paintVolume"/> <br/> <br/>
+
+                <label htmlFor="paintingTimes">Maalauskertojen määrä:</label> <br/>
+                <input type="text" id="paintingTimes" name="paintingTimes"/> <br/> <br/>
+
+                <input type="submit" name="getInfo" value="Lähetä tiedot" />
+            </form>
+        </div>
+    </div>
+  )
+}
+  
+export default App
