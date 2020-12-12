69f682ea0fb5d728d7e2f0c7fd8cbab211c7394c
noakarhu <59644045+noakarhu@users.noreply.github.com>
2020-12-13 00:15:32 +0200

    maalin hinta matikat

--- src/components/Maalilaskuri.js
+++ src/components/Maalilaskuri.js
@@ -4,56 +4,69 @@ import './Maalilaskuri.css'
 const Maalilaskuri = () => {
     const [submitting, setSubmitting] = useState(false);
     const [maalitiedot, setMaalitiedot] = useState({});
+    const [maalinHinta, setMaalinHinta] = useState(null);
 
     const handleSubmit = event => {
         event.preventDefault();
         setSubmitting(true);
+        setMaalinHinta(null);
 
         setTimeout(() => {
             setSubmitting(false);
-        }, 3000)
+            setMaalinHinta(laskeMaalinHinta(maalitiedot));
+        }, 2000)
         console.log('maalitiedot handleSubmit', maalitiedot);
+        console.log('maalinHinta', laskeMaalinHinta(maalitiedot));
+      
     }
 
     const handleMaalitiedotChange = (event) => {
         const fieldValue = event.target.value;
         const fieldName = event.target.name;
-        console.log('fieldValue', fieldValue);
-        console.log('fieldName', fieldName);
-        setMaalitiedot({...maalitiedot, [fieldName]: fieldValue});
+        setMaalitiedot({ ...maalitiedot, [fieldName]: fieldValue });
     }
 
+    const laskeMaalinHinta = (syötetytTiedot) => {
+        const litraHinta = syötetytTiedot.paintPrice || 0;
+        const maalausKerrat = syötetytTiedot.paintTimes || 1;
+        const maalinRiittoisuus = syötetytTiedot.paintVolume || 0;
 
-return (
-    <div>
-        <h1>Otsikko tulee tähän!</h1>
+        return (litraHinta + maalinRiittoisuus) * maalausKerrat;
+    }
 
-        <p>Sovellus tulostaa maalitarpeen litroina ja tämän litramäärän hinnan euroina koko taloyhtiölle, kullekkin asunnolle ja asuntojen eri tiloille.</p>
-        <p> </p>
-        <h2>Käytä maalilaskuria:</h2>
+    return (
+        <div>
+            <h1>Otsikko tulee tähän!</h1>
 
-        <div id="paintForm">
+            <p>Sovellus tulostaa maalitarpeen litroina ja tämän litramäärän hinnan euroina koko taloyhtiölle, kullekkin asunnolle ja asuntojen eri tiloille.</p>
+            <p> </p>
+            <h2>Käytä maalilaskuria:</h2>
+
+            <div id="paintForm">
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="paintName">Maalin nimi:</label> <br />
+                    <input type="text" id="paintName" name="paintName" onChange={handleMaalitiedotChange} /> <br /> <br />
+
+                    <label htmlFor="paintPrice">Maalin hinta (€/l):</label> <br />
+                    <input type="text" id="paintPrice" name="paintPrice" onChange={handleMaalitiedotChange} /> <br /> <br />
+
+                    <label htmlFor="paintVolume">Maalin riittoisuus (m<sup>2</sup>/l):</label> <br />
+                    <input type="text" id="paintVolume" name="paintVolume" onChange={handleMaalitiedotChange} /> <br /> <br />
+
+                    <label htmlFor="paintingTimes">Maalauskertojen määrä:</label> <br />
+                    <input type="text" id="paintingTimes" name="paintingTimes" onChange={handleMaalitiedotChange} /> <br /> <br />
+
+                    <button type="submit">Lähetä tiedot</button>
+                </form>
+            </div>
             {submitting &&
-            <div>Lähetetään tietoja...</div>
+                <div>Lähetetään tietoja...</div>
             }
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="paintName">Maalin nimi:</label> <br/>
-                <input type="text" id="paintName" name="paintName" onChange={handleMaalitiedotChange}/> <br/> <br/>
-
-                <label htmlFor="paintPrice">Maalin hinta (€/l):</label> <br/>
-                <input type="text" id="paintPrice" name="paintPrice" onChange={handleMaalitiedotChange}/> <br/> <br/>
-
-                <label htmlFor="paintVolume">Maalin riittoisuus (m<sup>2</sup>/l):</label> <br/>
-                <input type="text" id="paintVolume" name="paintVolume" onChange={handleMaalitiedotChange}/> <br/> <br/>
-
-                <label htmlFor="paintingTimes">Maalauskertojen määrä:</label> <br/>
-                <input type="text" id="paintingTimes" name="paintingTimes" onChange={handleMaalitiedotChange}/> <br/> <br/>
-
-                <button type="submit">Lähetä tiedot</button>
-            </form>
+            {maalinHinta && 
+                <h2>Maalin hinta: {maalinHinta} € </h2>
+            }
         </div>
-    </div>
-  )
+    )
 }
-  
+
 export default Maalilaskuri
